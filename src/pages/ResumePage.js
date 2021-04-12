27ae05bd291365abb201c193899786823d0604e0
Rantikaayuning <rantika97@gmail.com>
2021-04-12 14:05:10 +0700

    add view cv feature

--- src/pages/ResumePage.js
+++ src/pages/ResumePage.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Button, CardTitle, Row, Col, CardImg } from 'reactstrap';
 import Resume from '../assets/cvicon.jpg';
+import { jsPDF } from "jspdf";
+import Page1 from '../assets/page1.png';
+import Page2 from '../assets/page2.png';
 
 const ResumePage = () => {
+  const [isView, setView] = useState(false)
+
+  const handleView = () => {
+    const doc = new jsPDF();
+    doc.addImage(Page1, "PNG", 0, 0, 210, 297);
+    doc.addPage("a4", "m");
+    doc.addImage(Page2, "PNG", 0, 0, 210, 297);
+    window.open(doc.output('bloburl'))
+    setView(true)
+  }
+  
   return (
     <div className='resume-container'>
       <p>CV</p>
@@ -17,7 +31,11 @@ const ResumePage = () => {
           <Card body>
             <CardTitle tag="h5">CV</CardTitle>
             <CardImg top width="100%" height="50%" src={Resume} alt="Card image cap" /><br/>
-            <Button outline color='info'>Download</Button>
+            {isView ? (
+              <Button outline color='dark' disabled>View</Button>
+            ) : (
+              <Button outline color='info'onClick={() =>  handleView()}>View</Button>
+            )}
           </Card>
         </Col>
       </Row>
