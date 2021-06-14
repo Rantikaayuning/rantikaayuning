51d03bf7ff4a9e3d99c6fedc3607b566316f9b71
Rantikaayuning <rantika97@gmail.com>
2021-06-14 13:55:12 +0700

    updated styling layout width

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,36 +18,36 @@ const Header = () => {
           <ul className="nav__list grid">
             <li className="nav__item">
               <a href="#home" className="nav__link" onClick={toggle}>
-                <i className="bi bi-house-door nav__icon"></i> Home
+                <i class="uil uil-estate"></i> Home
               </a>
             </li>
             <li className="nav__item">
               <a href="#about" className="nav__link" onClick={toggle}>
-                <i className="bi bi-person nav__icon"></i> About
+                <i class="uil uil-user"></i> About
               </a>
             </li>
             <li className="nav__item">
               <a href="#skills" className="nav__link" onClick={toggle}>
-                <i className="bi bi-file-earmark nav__icon"></i> Skills
+                <i class="uil uil-desktop"></i> Skills
               </a>
             </li>
             <li className="nav__item">
               <a href="#portfolio" className="nav__link" onClick={toggle}>
-                <i className="bi bi-image nav__icon"></i> Portfolio
+                <i class="uil uil-file"></i> Portfolio
               </a>
             </li>
             <li className="nav__item">
               <a href="#contactme" className="nav__link" onClick={toggle}>
-                <i className="bi bi-envelope nav__icon"></i> Contactme
+                <i class="uil uil-navigator"></i> Contactme
               </a>
             </li>
           </ul>
-        <i className="bi bi-x-circle nav__close" onClick={toggle}></i>
+          <i class="uil uil-times-circle nav__close" onClick={toggle}></i>
         </div>
         
         <div className="nav-btns" onClick={toggle}>
           <div className="nav__toggle" id="nav-toggle">
-            <i className="bi bi-menu-button-wide"></i>
+            <i class="uil uil-bars nav__logo"></i>
           </div>
         </div>
       </nav>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,6 +8,7 @@ const HomePage = () => {
   const toggleSkills = () => {
     setSkills(!skills);
   }
+
   return (
     <main class="main">
       <section class="home section" id="home">
@@ -15,13 +16,13 @@ const HomePage = () => {
           <div class="home__content grid">
             <div class="home__social">
               <a href='https://github.com/Rantikaayuning' target='blank'>
-                <i class="bi bi-github"></i>
+                <i class="uil uil-github home__icon"></i>
               </a>
               <a href='https://www.linkedin.com/in/rantika-ayuning-b96563151/' target='blank'>
-                <i class="bi bi-linkedin"></i>
+                <i class="uil uil-linkedin home__icon"></i>
               </a>
               <a href='https://www.instagram.com/rantikaayuning' target='blank'>
-                <i class="bi bi-instagram"></i>
+                <i class="uil uil-instagram home__icon"></i>
               </a>
             </div>
 
@@ -36,29 +37,31 @@ const HomePage = () => {
                 Result-driven Front End Developer passionate about developing user-friendly website design.
               </p>
               <a href="#contact" class="button button--flex">
-                Contact Me <i class="bi bi-envelope button__icon"></i>
+                Contact Me <i class="uil uil-envelope button__icon"></i>
               </a>
             </div>
           </div>
 
           <div class="home__scroll">
             <a href="#about" class="home__scroll-button button--flex">
-              <i class="bi bi-mouse home__scroll-mouse"></i>
+              <i class="uil uil-mouse-alt home__scroll-mouse"></i>
               <span class="home__scroll-name">Scroll Down</span>
-              <i class="bi bi-arrow-down-circle home__scroll-arrow"></i>
+              <i class="uil uil-arrow-down home__scroll-arrow"></i>
             </a>
           </div>
         </div>
       </section>
 
-      <section class="about section">
+      <section class="about section" id="about">
         <h2 class="section__title">About Me</h2>
         <span class="section__subtitle">My Introduction</span>
         <div class="about__container container grid">
           <img src={Profile} alt="" class="about__img"/>
           <div class="about__data">
             <p class="about__description">
-              Front End Developer passionate about developing user-friendly website design. Excellent problem-solving skills and ability to perform well in a team.
+              Recently graduated from Glints Academy x Binar Academy coding bootcamp as Junior Front
+              End Developer with excellent problem-solving skills and ability to perform well in a team. I’m
+              seeking an entry-level position with Company to use my skills in coding.
             </p>
             <div class="about__info">
               <div>
@@ -72,9 +75,9 @@ const HomePage = () => {
             </div>
 
             <div class="about__buttons">
-              <a download="" href="./assets/resume-rantika.pdf" class="button button--flex">
-                Download Resume<i class="bi bi-download button__icon"></i>
-              </a>
+              <p class="button button--flex">
+                Download Resume<i class="uil uil-import button__icon"></i>
+              </p>
             </div>
           </div>
         </div>
@@ -86,12 +89,12 @@ const HomePage = () => {
         <div class="skills__container container grid">
           <div class={skills ? "skills__content skills__close" : "skills__content skills__open"}>
             <div class="skills__header">
-              <i class="bi bi-box skills__icon"></i>
+              <i class="uil uil-list-ul skills__icon"></i>
               <div>
                 <h1 class="skills__title">Frontend Developer</h1>
                 <span class="skills__subtitle">Certified as Junior Frontend Developer</span>
               </div>
-              <i class="bi bi-caret-down skills__arrow" onClick={toggleSkills}></i>
+              <i class="uil uil-angle-double-down skills__arrow" onClick={toggleSkills}></i>
             </div>
 
             <div class="skills__list grid">
@@ -143,6 +146,53 @@ const HomePage = () => {
         </div>
       </section>
 
+      <section class="qualification section">
+        <h2 class="section title">Qualification</h2>
+        <span class="section_subtitle">My Journey</span>
+        <div class="qualification__container container">
+          <div class="qualification__tabs">
+            <div class="qualification__button button--flex">
+              <i class="uil uil-graduation-cap qualification__icon"></i>Education
+            </div>
+
+            <div class="qualification__button button--flex">
+              <i class="uil uil-suitcase qualification__icon"></i>Work
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="qualification section">
+    <h2 class="section title">Qualification</h2>
+    <span class="section_subtitle">My Journey</span>
+    <div class="qualification__container container">
+      <div class="qualification__tabs">
+        <div class="qualification__button button--flex">
+          <i class="uil uil-graduation-cap qualification__icon"></i>Education
+        </div>
+
+        <div class="qualification__button button--flex">
+          <i class="uil uil-suitcase qualification__icon"></i>Work
+        </div>
+        
+        <div class="qualification__sections">
+          <div class="qualification__content">
+              <h3 class="qualification__data">
+                <div>
+                  <h3 class="qualification__title">Frontend Developer</h3>
+                  <span class="qualification__subtitle"></span>
+                  <div class="qualification__calendar">
+                    <i class="uil uil-calender"></i>
+                    2020 - 2021
+                  </div>
+                </div>
+
+              </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
     </main>
   );
