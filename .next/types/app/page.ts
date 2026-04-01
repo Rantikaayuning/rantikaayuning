555a55f0fae7d9af7d4c877fb6854e41d51500f8
Rantikaayuning <rantika97@gmail.com>
2026-04-01 17:54:47 +0700

    fix: error build

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\Rantika\OneDrive\Dokumen\Coding Apps\rantikaayuning\src\app\page.tsx
+// File: c:\Users\Rantika\OneDrive\Dokumen\Coding Apps\rantikaayuning\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
